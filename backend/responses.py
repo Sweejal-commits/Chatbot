b004ce82380d5d779b1b7745c1253900a0c326cf
Sweejal-commits <sweejalpanda335@gmail.com>
2025-02-17 18:42:28 +0530

    Frontend connected

--- backend/responses.py
+++ backend/responses.py
@@ -4,4 +4,7 @@ responses={
         "fee":"The fee structure depend on the course.Check here (https://www.imperial.edu.in/fee-details)",
         "syllabus":"You can download the syllabus from (https://www.imperial.edu.in/downloads/Syllabus#programme-syllabus)",
         "exam timetable":"The exam timetable is",
-        "holidays":"Check the academic calender "}
+        "holidays":"Check the academic calender ",
+        
+        #results,faculty,clubs&commity, library hours, canteen hours 
+        }
