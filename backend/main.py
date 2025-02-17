bd4e8235d13b41fbc7aa4d09204bc0a4bf76e7e2
Sweejal-commits <sweejalpanda335@gmail.com>
2025-02-17 19:17:29 +0530

    Update

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ import asyncio
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware, allow_origins=["chatbot-seetu.vercel.app"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"] )
+app.add_middleware(CORSMiddleware, allow_origins=["chatbot-seetu.vercel.app/chat.html"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"] )
 app.include_router(router)
 #Home endpoint(API info)
 @app.get("/")
