461b5ed3b93915a5250e6beab3341f25bb1a7544
Sweejal-commits <sweejalpanda335@gmail.com>
2025-02-20 21:53:48 +0530

    intents.py again

--- backend/nlp.py
+++ backend/nlp.py
@@ -1,6 +1,6 @@
 import spacy
 from rapidfuzz import process,fuzz
-from backend.responses import intents_responses
+from backend.intents import intents
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -12,20 +12,20 @@ def process_query(query:str):
 
 def detect_intent(user_input:str):  
     """Use fuzzy matching to find the best intent from intents.py"""
-    user_input= user_input.lower()
-    detected_intent = None
-    for intent, data in intents_responses.items():
-        if any (pattern in user_input for pattern in data["patterns"]):
-            detected_intent = intent
-            break
-        return detected_intent
+    # user_input= user_input.lower()
+    # detected_intent = None
+    # for intent, data in intents_responses.items():
+    #     if any (pattern in user_input for pattern in data["patterns"]):
+    #         detected_intent = intent
+    #         break
+    #     return detected_intent
 
-    # processed_input = process_query(user_input)
-    # best_intent = None
-    # best_score = 0
-    # for intent, phrases in intents.items():
-    #     match, score, _=process.extractOne(user_input,phrases,score = fuzz.partial_ratio)
-    #     if score > best_score:
-    #         best_match = intent
-    #         best_score = score
-    # return best_match if best_score > 50 else "unknown"  
+    processed_input = process_query(user_input)
+    best_intent = None
+    best_score = 0
+    for intent, phrases in intents.items():
+        match, score, _=process.extractOne(user_input,phrases,score = fuzz.partial_ratio)
+        if score > best_score:
+            best_match = intent
+            best_score = score
+    return best_match if best_score > 50 else "unknown"  
--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from backend.models import ChatRequest
-from backend.responses import intents_responses
+from backend.responses import responses
 from backend.nlp import detect_intent,process_query
 # from transformers import GPT2LMHeadModel, GPT2Tokenizer
 # import torch 
@@ -20,20 +20,20 @@ async def get_chat(query: str=""):
 @router.post("/chat")
 async def post_chat(request:ChatRequest):
     """Handles POST requests (JSON body)"""
-    user_input = request.get("message","").strip().lower() 
-    if not user_input:
-        return {"bot_response":"Hello!How can i assist you"}
-    response_text = process_query(user_input)
-    return{"bot_response":response_text}
-#     query = getattr(request,"user_input", "").strip().lower() 
-#     if not query:
-#           return {"bot_response":"Hello! How can i assist you today?"}
-#     #check predifined responses
-#     if query in responses:
-#         return{"bot_response":responses[query]}
-# #use nlp intent dectetion
-#     intent = detect_intent(query)
-#     # bot_response = detect_intent(query)
-#     return {"bot_response":responses.get(intent,"I'm not sure how to respond.")}
+    # user_input = request.get("message","").strip().lower() 
+    # if not user_input:
+    #     return {"bot_response":"Hello!How can i assist you"}
+    # response_text = process_query(user_input)
+    # return{"bot_response":response_text}
+    query = getattr(request,"user_input", "").strip().lower() 
+    if not query:
+          return {"bot_response":"Hello! How can i assist you today?"}
+    #check predifined responses
+    if query in responses:
+        return{"bot_response":responses[query]}
+#use nlp intent dectetion
+    intent = detect_intent(query)
+    # bot_response = detect_intent(query)
+    return {"bot_response":responses.get(intent,"I'm not sure how to respond.")}
 
 
