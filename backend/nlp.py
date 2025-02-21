e9e2b17c21a4058fb44c5b029465e138860c14d7
Sweejal-commits <sweejalpanda335@gmail.com>
2025-02-21 16:23:42 +0530

    NLP addition

--- backend/nlp.py
+++ backend/nlp.py
@@ -12,7 +12,7 @@ def process_query(query:str):
     return " ".join(keywords)
 
 def detect_intent(user_input:str):  
-    """Use fuzzy matching to find the best intent from intents.py"""
+    """Identify the best matching intent using spaCy similarity."""
     # user_input= user_input.lower()
     # detected_intent = None
     # for intent, data in intents_responses.items():
@@ -20,16 +20,27 @@ def detect_intent(user_input:str):
     #         detected_intent = intent
     #         break
     #     return detected_intent
-
-    processed_input = process_query(user_input)
-    # best_intent = None
-    # best_score = 0
+    processed_input = nlp(user_input.lower())
+    best_intent= "unknown"
+    best_score =0.0
     for intent, phrases in intents.items():
-        for phrase in phrases:
-            processed_phrase = process_query(phrase)
-            if processed_phrase in processed_input or processed_input in processed_phrase:
-                return intent
-        return "unknown"
+         for phrase in phrases:
+             phrase_doc = nlp(phrase.lower())
+             similarity = processed_input.similarity(phrase_doc)
+             if similarity > best_score and similarity >0.6:
+                 best_intent= intent
+                 best_score= similarity
+    return best_intent
+    # """Identify the best matching intent using """
+    # # (processed_input = process_query(user_input)
+    # # best_intent = None
+    # # best_score = 0
+    # for intent, phrases in intents.items():
+    #     for phrase in phrases:
+    #         processed_phrase = process_query(phrase)
+    #         if processed_phrase in processed_input or processed_input in processed_phrase:
+    #             return intent
+    #     return "unknown")
     #     match, score, _=process.extractOne(user_input,phrases,score = fuzz.partial_ratio)
     #     if score > best_score:
     #         best_match = intent
@@ -42,6 +53,6 @@ def get_response(user_input:str):
         response = responses[intent]
         return response if isinstance(response,str) else response[0]
     return response["unknown"]
-if __name__=="__main__":
-    user_input = input("You:")
-    print("Bot:", get_response(user_input))
+# if __name__=="__main__":
+#     user_input = input("You:")
+#     print("Bot:", get_response(user_input))
